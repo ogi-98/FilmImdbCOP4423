da64507b17d2689d6d57d11271f44ef2b7c6320c
ogi-98 <oguzkaya99@hotmail.com>
2021-11-15 15:19:28 +0300

    Navigation & Toast Message

--- app/src/main/java/com/example/filmimdbcop4423/MainActivity.java
+++ app/src/main/java/com/example/filmimdbcop4423/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.filmimdbcop4423;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Welcome to MyApp", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
